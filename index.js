dba2ac4d97f52f65b46c89ee513f3af068d3d484
Daniil Gavrilov <dp070593gda@gmail.com>
2020-08-12 04:40:42 +0300

    Base script for parse all pages

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 const URL = 'https://classinform.ru/profstandarty.html';
 const results = [];
 const q = tress(work);
-q.drain = done;
+q.success = done;
 start();
 
 
@@ -16,53 +16,55 @@ function start() {
     needle.get(URL, function(err, res) {
         if (err) throw err;
         const $ = cheerio.load(res.body);
-        $('.full_width .four_fifth > a').each(function() {
-            q.push(resolve(URL, $(this).attr('href')))
+        $('.full_width > .four_fifth > a').each(function() {
+            
+            needle.get(resolve(URL, $(this).attr('href')), function(err, res) {
+                if (err) throw err;
+                const $ = cheerio.load(res.body);
+                $('.full_width > .one_fifth > a').each(function() {
+                    q.push(resolve(URL, $(this).attr('href')));
+                });
+                
+            });
         });
+
     });
 }
 function work(url, cb) {
-    needle.get(url, function(err, res){
-        if (err) throw err;
-
+    needle('get', url).then(function(res){
         const $ = cheerio.load(res.body);
-        const subEl = [];
-        $('.full_width > .one_fifth > a').each(function() {
-            const table = [];
-                const subUrl = resolve(URL, $(this).attr('href'));
-                needle.get(subUrl, function(err, res) {
-                    const $ = cheerio.load(res.body);
-                    
-                    $('td:first-child > *:contains(Возможные)').each(function() {
-                        table.push($(this).parent().next().children().text().trim());
-                    });
-                    console.log(subUrl);
-                    console.log('Таблица',table);
-                });
-
+        const table = [];
+        if($('td:first-child > *:contains(Возможные)')) {
+            $('td:first-child > *:contains(Возможные)').each(function() {
+                table.push($(this).parent().next().children().text().trim());
+            });
+        }
+        if($('td:first-child :contains(Возможные)')) {
+            $('td:first-child :contains(Возможные)').each(function() {
+                table.push($(this).parent().next().children().text().trim());
+            });
+        }
         
-            const el = {
-                code: $(this).text().trim(),
-                name: $(this).parent().next().children().text().trim(),
-                url: resolve(URL, $(this).attr('href')),
-                table: table
-            }
-            subEl.push(el);
-        });
+        console.log(url);
+        console.log('Таблица',table);
+
         const i = {
+            parentName: $('#cont_txt >.full_width:nth-child(5) > .path > .four_fifth > a').text().trim(),
+            parentCode: $('#cont_txt >.full_width:nth-child(5) > .path > .one_fifth > a').text().trim(),
             name: $('.full_width > .four_fifth > h3').text().trim(),
             code: $('.full_width > .one_fifth > h3').text().trim(),
-            subEl: subEl
+
         }
         results.push(i);
         cb();
     });
+  
+    
+
 };
 
 
 function done(){
-    console.log(results);
+    // console.log(results);
+    fs.writeFileSync('./data.json', JSON.stringify(results, null, 4));
 }
-
-fs.writeFileSync('./data.json', JSON.stringify(results, null, 4));
-
